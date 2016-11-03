f1c98d200b5beb698a3ccd82928f6bf1f2c1eb89
jackillman <jackillman@gmail.com>
2016-11-03 13:49:07 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
-<?php
-
-$recepient = "jackillman@gmail.com";
-$sitename = "Мой мастер";
-
-$name = trim($_GET["name"]);
-$phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
-
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+<?
+if (array_key_exists('messageFF', $_POST)) {
+   $to = 'jackillman@gmail.com';
+   $subject = 'Заполнена контактная форма с '.$_SERVER['HTTP_REFERER'];
+   $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
+   $message = "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nСообщение: ".$_POST['messageFF'];
+   $headers = 'Content-type: text/plain; charset="utf-8"';
+   $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
+   mail($to, $subject, $message, $headers);
+   echo $_POST['nameFF'];
+}
+?>
